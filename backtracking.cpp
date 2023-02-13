9ca74c45cde90e81856170893fdd5c1adfeb10ac
Karan Kumar Mishra <93134411+Karan-Kumar-Mishra@users.noreply.github.com>
2023-02-13 13:32:22 +0530

    Add files via upload

---
+++ backtracking.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+using namespace std;
+//backtracking
+//rat maze
+bool isSafe(int** arr,int x,int y,int n)
+{
+    if(x<n && y <n && arr[x][y]==1)
+    {
+        return 1;
+    }
+    return 0;
+}
+bool ratMaze(int ** arr,int x,int y,int n,int** solArr)
+{
+    //base case
+    if(x==n-1 && y==n-1)
+    {
+        solArr[x][y]=1;
+        return  1;
+    }
+    if(isSafe(arr,x,y,n))
+    {
+        solArr[x][y]=1;
+
+        if(ratMaze(arr,x+1,y,n,solArr))
+        {
+            return 1;
+        }
+        if(ratMaze(arr,x,y+1,n,solArr))
+        {
+            return 1;
+        }
+        solArr[x][y]=0;
+        return 0;
+    }
+    return 0;
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int** arr= new int*[n];
+    for(int i=0; i<n; i++)
+    {
+        arr[i]=new int[n];
+    }
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    int **solArr= new int*[n];
+    for(int i=0; i<n; i++)
+    {
+        solArr[i]=new int[n];
+        for(int j=0; j<n; j++)
+        {
+            solArr[i][j]=0;
+        }
+    }
+    if(ratMaze(arr,0,0,n,solArr))
+    {
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                cout<<solArr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
